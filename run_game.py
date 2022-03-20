ec592122e309b2db32edaf4e91d3f7ead3dff192
Robert O'Shea <robertoshea2k11@gmail.com>
2022-03-20 02:06:37 +0000

    Initial commit

---
+++ run_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import arcade
+
+from trevor.game import Game
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Starting Template"
+
+if __name__ == "__main__":
+    game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
---
+++ trevor/game.py
@@ -0,0 +1,44 @@
+import math
+import arcade
+
+class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.wood_sprite = None
+        self.sword_sprite = None
+        self.sprite_list = None
+        self.setup()
+
+    def setup(self):
+        self.sprite_list = arcade.SpriteList()
+        self.wood_sprite = arcade.Sprite("assets/images/wood.png", 1.5)
+        self.wood_sprite.position = (100, 100)
+        self.sword_sprite = arcade.Sprite("assets/images/ironsword.png", 1.5)
+        self.sword_sprite.position = (200, 100)
+        
+        self.sprite_list.append(self.wood_sprite)
+        self.sprite_list.append(self.sword_sprite)
+
+    def on_draw(self):
+        self.clear()
+        self.sprite_list.draw()
+
+    def on_update(self, delta_time):
+        self.sword_sprite.angle = (self.sword_sprite.angle + 5) % 360
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        pass
