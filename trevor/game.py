7fa3aff56edb4ceb772202f6d1d82b40331448db
Robert O'Shea <robertoshea2k11@gmail.com>
2022-03-21 15:01:02 +0000

    Removed unused import

--- trevor/game.py
+++ trevor/game.py
@@ -1,13 +1,13 @@
 import arcade
-import arcade.gui as gui
 
+from .scenes.trevorscene import TrevorScene
 from .scenes.gamescene import MainGameScene
 
 class Game(arcade.Window):
-    def __init__(self, width, height, title):
+    def __init__(self, width: int, height: int, title: str):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.AMAZON)
-        self.scene = None
+        self.scene: TrevorScene = None
         self.setup()
 
     def setup(self):
@@ -17,20 +17,20 @@ class Game(arcade.Window):
         self.clear()
         self.scene.draw()
 
-    def on_update(self, delta_time):
+    def on_update(self, delta_time: float):
         self.scene.on_update(delta_time)
 
-    def on_key_press(self, key, key_modifiers):
+    def on_key_press(self, key: int, key_modifiers: int):
         self.scene.on_key_press(key, key_modifiers)
 
-    def on_key_release(self, key, key_modifiers):
+    def on_key_release(self, key: int, key_modifiers: int):
         self.scene.on_key_release(key, key_modifiers)
 
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
+    def on_mouse_motion(self, x: float, y: float, delta_x: float, delta_y: float):
         self.scene.on_mouse_motion(x, y, delta_x, delta_y)
 
-    def on_mouse_press(self, x, y, button, key_modifiers):
+    def on_mouse_press(self, x: float, y: float, button, key_modifiers):
         self.scene.on_mouse_press(x, y, button, key_modifiers)
 
-    def on_mouse_release(self, x, y, button, key_modifiers):
+    def on_mouse_release(self, x: float, y: float, button: int, key_modifiers: int):
         self.scene.on_mouse_release(x, y, button, key_modifiers)
