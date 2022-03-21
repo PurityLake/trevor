3750bba4f73de949e22e0a39f636c64b31498577
Robert O'Shea <robertoshea2k11@gmail.com>
2022-03-21 00:58:25 +0000

    Can now change number of hearts of player

--- trevor/scenes/gamescene.py
+++ trevor/scenes/gamescene.py
@@ -14,10 +14,21 @@ class MainGameScene(arcade.Scene, TrevorScene):
         super().__init__()
 
         self.player = Player()
+        self.last_health = self.player.health
         self.sprite_list = arcade.SpriteList()
         self.sprite_list.append(self.player)
-        self.add_sprite_list(self.sprite_list)
+        self.add_sprite_list("Player")
+        self.add_sprite("Player", self.player)
 
+        self.manager = None
+        self.hbox = None
+        self.heartfulltex = None
+        self.heartemptytex = None
+        self.heartwidgets = None
+        self.__setup_gui()
+        
+
+    def __setup_gui(self):
         self.manager = gui.UIManager()
         self.manager.enable()
 
@@ -29,24 +40,25 @@ class MainGameScene(arcade.Scene, TrevorScene):
         for i in range(Player.MAX_HEALTH):
             if i < self.player.health:
                 widget = gui.UISpriteWidget(
-                    x=20+i*64, y=HEART_HEIGHT,
+                    x=i*HEART_WIDTH, y=HEART_HEIGHT,
                     width=HEART_WIDTH, height=HEART_HEIGHT,
                     sprite=self.heartfulltex
                 )
-                self.heartwidgets.append(widget.with_space_around(right=5, bottom=10))
+                self.heartwidgets.append(widget.with_space_around(left=5, bottom=10))
             else:
-                widget = widget = gui.UISpriteWidget(
-                    x=20+i*64, y=HEART_HEIGHT,
+                widget = gui.UISpriteWidget(
+                    x=i*HEART_WIDTH, y=HEART_HEIGHT,
                     width=HEART_WIDTH, height=HEART_HEIGHT,
                     sprite=self.heartemptytex
                 )
-                self.heartwidgets.append(widget.with_space_around(right=5, bottom=10))
+                self.heartwidgets.append(widget.with_space_around(left=5, bottom=10))
             self.hbox.add(self.heartwidgets[i])
         
         self.manager.add(
             gui.UIAnchorWidget(
                 anchor_x="left",
                 anchor_y="bottom",
+                align_x=10,
                 child=self.hbox
             )
         )
@@ -57,9 +69,38 @@ class MainGameScene(arcade.Scene, TrevorScene):
     
     def on_update(self, delta_time):
         self.player.update(delta_time)
+
+        if self.player.health != self.last_health:
+            diff = self.last_health - self.player.health
+            sprite = self.heartemptytex if diff > 0 else self.heartfulltex
+
+            if diff > 0:
+                for i in range(self.player.health, self.last_health):
+                    widget = gui.UISpriteWidget(
+                        x=i*HEART_WIDTH, y=HEART_HEIGHT,
+                        width=HEART_WIDTH, height=HEART_HEIGHT,
+                        sprite=sprite
+                    )
+                    self.hbox.children[max(0,i)] = widget.with_space_around(left=5, bottom=10)
+            else:
+                for i in range(self.last_health, self.player.health):
+                    widget = gui.UISpriteWidget(
+                        x=i*HEART_WIDTH, y=HEART_HEIGHT,
+                        width=HEART_WIDTH, height=HEART_HEIGHT,
+                        sprite=sprite
+                    )
+                    self.hbox.children[min(Player.MAX_HEALTH-1,i)] = widget.with_space_around(left=5, bottom=10)
+            self.last_health = self.player.health
+
     
     def on_key_press(self, key, key_modifiers):
         self.player.key_press(key, key_modifiers)
+
+        if key == arcade.key.LEFT:
+            self.player.health = max(0, self.player.health - 1)
+        
+        if key == arcade.key.RIGHT:
+            self.player.health = min(self.player.health + 1, Player.MAX_HEALTH)
     
     def on_key_release(self, key, key_modifiers):
         self.player.key_release(key, key_modifiers)
